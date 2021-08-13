d40ef6fa717f44b1e08040ec264fd98a4d236023
minna <yakubuaminat94@gmail.com>
2021-08-13 02:34:04 +0100

    feat: sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
+import { Windmill } from '@windmill/react-ui'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <Windmill>
+        <Component {...pageProps} />
+      </Windmill>
     </UserProvider>
     
   )
