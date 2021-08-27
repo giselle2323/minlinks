2815d40158b6357fe6febc047150c2674b263610
giselle2323 <yakubuaminat94@gmail.com>
2021-08-27 18:18:21 +0100

    feat: infinite scroll

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,19 +1,61 @@
 import React, {useState} from 'react';
 import { QueryClient, useQuery } from 'react-query'
-import { dehydrate } from 'react-query/hydration'
+import { dehydrate } from 'react-query/hydration';
+import InfiniteScroll from "react-infinite-scroll-component";
 import {fetchArticlesIdeas} from '../hooks/useArticles';
+import { getMoreArticles } from '../hooks/useGetMorePosts';
 import Admin from "../layouts/Admin";
 import ArticleCard from "../components/Cards/Card";
+import { supabase } from '../utils/supabase';
 export default function Dashboard () {
-    const { data: articlesIdeas } = useQuery('articlesIdeas', fetchArticlesIdeas);
-    console.log(articlesIdeas.map(idea => (
-      console.log(idea)
-    )))
-
+    const [ideas, setIdeas] = useState(useQuery('articlesIdeas', fetchArticlesIdeas));
+    const [hasMore, setHasMore] = useState(true);
+    const [rangeNumber, setRangeNumber] = useState(20);
+    
+    const getMoreArticles = async () => {
+      const { data, error } = await supabase
+      .from('posts')
+      .select(`
+        id, 
+        title, 
+        body, 
+        tag, 
+        links,
+        authorId(id, name),
+        comments(
+          id, 
+          body,
+          userId(id, name)
+        ),
+        likes(
+          id,
+          postId,
+          userId(id, name)
+        )
+    
+      `)
+      .range(rangeNumber, rangeNumber + 10)
+      
+      if(error) {
+        throw new Error(error.message)
+      }
+      setRangeNumber(rangeNumber + 20);
+      setIdeas((ideas) => [...ideas, ...data]);
+      return data;
+    };
+  
+console.log(ideas)
     return (
     <main className="flex flex-col w-full">
+      <InfiniteScroll
+        dataLength={50}
+        next={getMoreArticles}
+        hasMore={hasMore}
+        loader={<h3> Loading...</h3>}
+        endMessage={<h4>Nothing more to show</h4>}
+      >
         <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {articlesIdeas.map(({title, id, body, comments, likes}) => (
+            {ideas.data.map(({title, id, body, comments, likes}) => (
                 <ArticleCard 
                   key={id}  
                   title={title} 
@@ -23,6 +65,8 @@ export default function Dashboard () {
                 />
             ))}
         </ul>
+      </InfiniteScroll>
+        
     </main>
     )
 }
@@ -39,4 +83,4 @@ export async function getStaticProps() {
         dehydratedState: dehydrate(queryClient),
       },
     }
-  }
+}
